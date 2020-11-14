4cf5c850cbec20e73e0cc28454824120d547eeeb
Kotaro-Kitafusa <puppet0pierrot@gmail.com>
2020-11-14 16:12:41 +0900

    ヘッダーふっだーを表示させる

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,28 @@
 <template lang="pug">
+  div
+    header
+      .title
+        h1 NES-lab
+      .menu
+        ul
+         li
+          a(href="#") about
+         li
+          a(href="#") chat
+
+    nuxt.content-area
+
+    footer
+      h1 Video Title
 
 </template>
 
 <style lang="scss" scoped>
-
+  @import '../assets/css/header_footer';
 </style>
 
 <script lang="ts">
   import { Component, Vue } from 'nuxt-property-decorator';
+  @Component
+  export default class DefaultLayout extends Vue {}
 </script>
